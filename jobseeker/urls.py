675e7818762076f477a7a59a1b052a1120493f94
adulnishad <adulnishad22@gmail.com>
2023-12-21 15:07:22 +0530

    add

---
+++ jobseeker/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from jobseeker import views
+
+urlpatterns = [
+
+    path("register/",views.SignUpView.as_view(),name='signup'),
+    path("index",views.StudentIndexView.as_view(),name='seeker-index'),
+    path("profile/add",views.ProfileCreateView.as_view(),name="profile-add"),
+    path("profile/<int:pk>",views.ProfileDetailView.as_view(),name="profile-detail"),
+    path("profile/<int:pk>/change",views.profileEditView.as_view(),name="profile-edit"),
+    path("jobs/<int:pk>",views.JobDetailView.as_view(),name='job-detail'),
+    path("job/<int:pk>/apply",views.ApplyJob.as_view(),name='job-apply'),
+    path("applications/all",views.ApplicationsListView.as_view(),name='myapplications'),
+    path("jobs/<int:pk>/save",views.jobSaveView.as_view(),name="job-save"),
+    path("jobs/saved/all",views.SavedJobsListView.as_view(),name="saved-jobs"),
+    
+
+]
---
+++ jobseeker/views.py
@@ -0,0 +1,106 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
+from django.views.generic import View,CreateView,TemplateView,DetailView,UpdateView,ListView
+
+
+
+from jobseeker.forms import Registration,ProfileForm
+from myapp.models import StudentProfile,Jobs,Applications
+
+# Create your views here.
+
+class SignUpView(CreateView):
+    template_name="jobseeker/registrion.html"
+    form_class=Registration
+    success_url=reverse_lazy("signin")
+
+
+class StudentIndexView(ListView):
+    template_name="jobseeker/index.html"
+    context_object_name="data"
+    model=Jobs
+    
+
+
+    def get_queryset(self):
+        my_applications=Applications.objects.filter(student=self.request.user).values_list("job",flat=True)
+        qs=Jobs.objects.exclude(id__in=my_applications).order_by("-Create_date")
+        return qs
+
+
+class ProfileCreateView(CreateView):
+    template_name="jobseeker/profile_add.html"
+    form_class=ProfileForm
+    success_url=reverse_lazy("seeker-index")
+
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+    
+        return super().form_valid(form)
+
+
+class ProfileDetailView(DetailView):
+    template_name="jobseeker/profile_detail.html"
+    context_object_name="data"
+    model=StudentProfile
+
+
+class profileEditView(UpdateView):
+    template_name="jobseeker/Profile_edit.html"
+    form_class=ProfileForm
+    model=StudentProfile
+    success_url=reverse_lazy("seeker-index")
+
+
+class JobDetailView(DetailView):
+    template_name="jobseeker/job_detail.html"
+    model=Jobs
+    context_object_name="data"
+
+
+class ApplyJob(View):
+    def post(self,request,*args,**kwargs): 
+        id=kwargs.get("pk")
+        job_object=Jobs.objects.get(id=id)
+        student_object=request.user
+        Applications.objects.create(job=job_object,student=student_object)
+        return redirect('seeker-index')
+    
+
+class ApplicationsListView(ListView):
+    template_name="jobseeker/application.html"
+    model=Applications
+    context_object_name="data"
+
+    def get_queryset(self):
+        qs=Applications.objects.filter(student=self.request.user)
+        return qs
+    
+
+
+class jobSaveView(View):
+    def post(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        job_object=Jobs.objects.get(id=id)
+        action=request.POST.get("action")
+        if action=="save":
+            request.user.profile.saved_jobs.add(job_object)
+        elif action=="Unsave":
+            request.user.profile.saved_jobs.remove(job_object)
+        return redirect("seeker-index")
+
+class SavedJobsListView(View):
+    template_name="jobseeker/saved_jobs.html"
+    model=StudentProfile
+    def get(self,request,*args,**kwargs):
+        qs=request.user.profile.saved__jobs.all()
+        return render(request,self.template_name,{"data":qs})
+    
+# class JobListview(ListView):
+#     template_name="jobseeker/Job_list.html"
+#     context_object_name="jobs"
+#     model=Jobs
